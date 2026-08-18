308898aec648051b9ff640f880b986bf15511d04
Roman Matviy <roman@matviy.pp.ua>
2026-08-18 19:57:56 +0300

    feat: implement sitemap generation utilities and locale-based redirect middleware

--- lib/sitemap-utils.ts
+++ lib/sitemap-utils.ts
@@ -12,7 +12,7 @@ export function escapeXml(unsafe: string): string {
       case '<': return '&lt;';
       case '>': return '&gt;';
       case '&': return '&amp;';
-      case '\\'': return '&apos;';
+      case "'": return '&apos;';
       case '"': return '&quot;';
       default: return c;
     }
